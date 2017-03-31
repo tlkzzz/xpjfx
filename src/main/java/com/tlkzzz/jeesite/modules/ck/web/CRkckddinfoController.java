f5b297186dab95566dd51e3efd873b07413f779b
shizhenxin <821043672@qq.com>
2017-03-31 11:57:09 +0800

    szx

--- src/main/java/com/tlkzzz/jeesite/modules/ck/web/CRkckddinfoController.java
+++ src/main/java/com/tlkzzz/jeesite/modules/ck/web/CRkckddinfoController.java
@@ -1,5 +1,5 @@
 /**
- * Copyright &copy; 2012-2016 <a href="https://github.com/tlkzzz/jeesite">JeeSite</a> All rights reserved.
+ * Copyright &copy; 2012-2016 <a href="https://github.com/tlkzzz/jeesite">JeeSite</a> All rights reserved.s
  */
 package com.tlkzzz.jeesite.modules.ck.web;
 
