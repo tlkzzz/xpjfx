707d4ba918fb3572c00a300a072fb6e251c6a952
BearBigBigBig <vip-xiong@foxmail.com>
2017-04-10 19:53:12 +0800

    xrc

--- src/main/java/com/tlkzzz/jeesite/modules/cw/web/FTransferAccountController.java
+++ src/main/java/com/tlkzzz/jeesite/modules/cw/web/FTransferAccountController.java
@@ -6,6 +6,10 @@ package com.tlkzzz.jeesite.modules.cw.web;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import com.tlkzzz.jeesite.modules.cw.entity.FReceipt;
+import com.tlkzzz.jeesite.modules.cw.service.FReceiptService;
+import com.tlkzzz.jeesite.modules.sys.utils.UserUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +17,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.tlkzzz.jeesite.common.config.Global;
@@ -33,6 +38,8 @@ public class FTransferAccountController extends BaseController {
 
 	@Autowired
 	private FTransferAccountService fTransferAccountService;
+	@Autowired
+	private FReceiptService fReceiptService;
 	
 	@ModelAttribute
 	public FTransferAccount get(@RequestParam(required=false) String id) {
@@ -61,7 +68,25 @@ public class FTransferAccountController extends BaseController {
 		return "modules/cw/fTransferAccountForm";
 	}
 
-	@RequiresPermissions("cw:fTransferAccount:edit")
+	/**
+	 * 应付款增加页面
+	 * @param transferAccount
+	 * @param model
+	 * @return
+	 */
+//	@RequiresPermissions("cw:fTransferAccount:view")
+	@RequestMapping(value = "receiptForm")
+	public String receiptForm(FTransferAccount transferAccount, Model model){
+		if(StringUtils.isNotBlank(transferAccount.getTransferType())) {
+			model.addAttribute("fTransferAccount", transferAccount);
+			model.addAttribute("orderList", fReceiptService.findList(new FReceipt()));
+			return "modules/cw/fTransferReceiptForm";
+		}else {
+			return "error/400";
+		}
+	}
+
+//	@RequiresPermissions("cw:fTransferAccount:edit")
 	@RequestMapping(value = "save")
 	public String save(FTransferAccount fTransferAccount, Model model, RedirectAttributes redirectAttributes) {
 		if (!beanValidator(model, fTransferAccount)){
@@ -78,6 +103,38 @@ public class FTransferAccountController extends BaseController {
 		fTransferAccountService.delete(fTransferAccount);
 		addMessage(redirectAttributes, "删除转账调账成功");
 		return "redirect:"+Global.getAdminPath()+"/cw/fTransferAccount/?repage";
+	}
+
+	@ResponseBody
+	@RequiresPermissions("user")
+	@RequestMapping(value = "auditing")
+	public String auditing(String id,String state) {
+		String retStr = "false";
+		if(StringUtils.isNotBlank(id)&&StringUtils.isNotBlank(state)) {
+			FTransferAccount transferAccount = new FTransferAccount(id);
+			transferAccount.setApprovalStatus(state);
+			transferAccount.setAuditor(UserUtils.getUser());
+			fTransferAccountService.updateApprovalState(transferAccount);
+			transferAccount = fTransferAccountService.get(transferAccount);
+			if(transferAccount!=null) {
+				switch (Integer.parseInt(state)) {
+					case 1: {
+						FReceipt receipt = new FReceipt(transferAccount.getOrderId());
+						receipt.setHtje(transferAccount.getTransMoney());
+						fReceiptService.addHTJE(receipt);
+						break;
+					}
+					case 2: {
+						FReceipt receipt = new FReceipt(transferAccount.getOrderId());
+						receipt.setHtje(transferAccount.getTransMoney());
+						fReceiptService.minHTJE(receipt);
+						break;
+					}
+				}
+			}
+			retStr = "true";
+		}
+		return retStr;
 	}
 
 }
