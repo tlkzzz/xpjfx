e7287a6ced187f6988b3d018e99027819e034201
BearBigBigBig <vip-xiong@foxmail.com>
2017-04-26 15:14:17 +0800

    xrc

--- src/main/java/com/tlkzzz/jeesite/modules/ck/entity/CDdinfo.java
+++ src/main/java/com/tlkzzz/jeesite/modules/ck/entity/CDdinfo.java
@@ -45,6 +45,10 @@ public class CDdinfo extends DataEntity<CDdinfo> {
 	private String sfkId;     //收付款表ID
 	private FAccount fAccount;   //账号信息
 
+	private Date startDate;		//开始时间
+	private Date endDate;		//结束时间
+	private String type;		//报表类型
+
 	public CDdinfo() {
 		super();
 	}
@@ -241,5 +245,29 @@ public class CDdinfo extends DataEntity<CDdinfo> {
 
 	public void setfAccount(FAccount fAccount) {
 		this.fAccount = fAccount;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
 	}
 }
