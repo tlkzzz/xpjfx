91d65f54e0e8e5baced469f12c6b9ed30e74b3f6
xlc <xlc1994@qq.com>
2017-06-19 20:52:55 +0800

    xlc

--- src/main/java/com/tlkzzz/jeesite/modules/test/web/TestController.java
+++ src/main/java/com/tlkzzz/jeesite/modules/test/web/TestController.java
@@ -6,14 +6,21 @@ package com.tlkzzz.jeesite.modules.test.web;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.tlkzzz.jeesite.common.config.Global;
+import com.tlkzzz.jeesite.common.utils.FreeMarkers;
+import com.tlkzzz.jeesite.common.utils.IdGen;
 import com.tlkzzz.jeesite.common.utils.StringUtils;
 import com.tlkzzz.jeesite.common.web.BaseController;
 import com.tlkzzz.jeesite.modules.sys.entity.User;
+import com.tlkzzz.jeesite.modules.sys.utils.DictUtils;
 import com.tlkzzz.jeesite.modules.sys.utils.UserUtils;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import org.apache.commons.collections.map.HashedMap;
+import org.apache.commons.io.output.FileWriterWithEncoding;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.interceptor.TransactionAspectSupport;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +31,10 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.tlkzzz.jeesite.common.persistence.Page;
 import com.tlkzzz.jeesite.modules.test.entity.Test;
 import com.tlkzzz.jeesite.modules.test.service.TestService;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 测试Controller
@@ -125,6 +136,71 @@ public class TestController extends BaseController {
 //		addMessage(redirectAttributes, "删除测试成功");
 //		return "redirect:" + adminPath + "/test/test/?repage";
 		return "true";
+	}
+
+	@RequiresPermissions("test:test:edit")
+	@RequestMapping(value = "freemarkTest")
+	public void freemarkTest(HttpServletRequest request,HttpServletResponse response){
+		Map<String, Object> model = new HashMap();
+		model.put("userName", "XRC");
+
+
+		String content = FreeMarkers.renderFile("/WEB-INF/views/modules/test","index.ftl", request, model);//利用模版生成html页面
+
+		String contentTow = FreeMarkers.renderFile("/WEB-INF/views/static","index.html", request, model);//利用html文件生成页面
+
+		renderString(response,contentTow,"text/html; charset=UTF-8");
+	}
+	@RequiresPermissions("test:test:edit")
+	@RequestMapping(value = "lsit")
+	public void lsit(HttpServletRequest request,HttpServletResponse response){
+		Map<String, Object> model = new HashMap();
+		String contentTow = FreeMarkers.renderFile("/WEB-INF/views/static","cwgl.html", request, model);//利用html文件生成页面
+		renderString(response,contentTow,"text/html; charset=UTF-8");
+	}
+	@RequiresPermissions("test:test:edit")
+	@RequestMapping(value = "lsitq")
+	public void lsitq(HttpServletRequest request,HttpServletResponse response){
+		Map<String, Object> model = new HashMap();
+		String contentTow = FreeMarkers.renderFile("/WEB-INF/views/static","cxyw.html", request, model);//利用html文件生成页面
+		renderString(response,contentTow,"text/html; charset=UTF-8");
+	}
+	@RequiresPermissions("test:test:edit")
+	@RequestMapping(value = "lsitqw")
+	public void lsitqw(HttpServletRequest request,HttpServletResponse response){
+		Map<String, Object> model = new HashMap();
+		String contentTow = FreeMarkers.renderFile("/WEB-INF/views/static","dbsy.html", request, model);//利用html文件生成页面
+		renderString(response,contentTow,"text/html; charset=UTF-8");
+	}
+	@RequiresPermissions("test:test:edit")
+	@RequestMapping(value = "lsitqwe")
+	public void lsitqwe(HttpServletRequest request,HttpServletResponse response){
+		Map<String, Object> model = new HashMap();
+		String contentTow = FreeMarkers.renderFile("/WEB-INF/views/static","jybg.html", request, model);//利用html文件生成页面
+		renderString(response,contentTow,"text/html; charset=UTF-8");
+	}
+	@RequiresPermissions("test:test:edit")
+	@RequestMapping(value = "lsitqwer")
+	public void lsitqwer(HttpServletRequest request,HttpServletResponse response){
+		Map<String, Object> model = new HashMap();
+		String contentTow = FreeMarkers.renderFile("/WEB-INF/views/static","kcgl.html", request, model);//利用html文件生成页面
+		renderString(response,contentTow,"text/html; charset=UTF-8");
+	}
+	@RequiresPermissions("test:test:edit")
+	@RequestMapping(value = "lsitqwert")
+	public void lsitqwert(HttpServletRequest request,HttpServletResponse response){
+		Map<String, Object> model = new HashMap();
+		String contentTow = FreeMarkers.renderFile("/WEB-INF/views/static","ysyw.html", request, model);//利用html文件生成页面
+		renderString(response,contentTow,"text/html; charset=UTF-8");
+	}
+	@RequestMapping(value = "shopTest")
+	public void shopTest(HttpServletRequest request,HttpServletResponse response){
+		Map<String, Object> model = new HashMap();
+		model.put("ctx", request.getContextPath()+Global.getAdminPath());
+		model.put("ctxStatic", request.getContextPath()+"/static");
+		model.put("userName", "XRC");
+		String contentTow = FreeMarkers.renderFile("/WEB-INF/views/static/one","login.html",request, model);//利用html文件生成页面
+		renderString(response,contentTow,"text/html;charset=UTF-8");
 	}
 
 }
