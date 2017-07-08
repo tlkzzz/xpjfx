63dbdcfbae1d40be7b131ccb4714ea195123b479
shizhenxin <821043672@qq.com>
2017-07-08 15:25:33 +0800

    szx77

--- src/main/java/com/tlkzzz/jeesite/modules/ck/entity/CHgoods.java
+++ src/main/java/com/tlkzzz/jeesite/modules/ck/entity/CHgoods.java
@@ -16,7 +16,7 @@ import com.tlkzzz.jeesite.common.persistence.DataEntity;
 /**
  * 仓库商品Entity
  * @author xrc
- * @version 2017-03-13
+ * @version 2017-03-13sss
  */
 public class CHgoods extends DataEntity<CHgoods> {
 
